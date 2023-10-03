a35f71f41b10a73408a6224d0a5b25e25ebd4315
AlgoStar0418 <bs07120418@gmail.com>
2023-10-03 10:34:39 +0200

    [update] genereated requirements.txt

--- server/AIagent/projects/serializers.py
+++ server/AIagent/projects/serializers.py
@@ -5,6 +5,7 @@ from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ['uid', 'name', 'type', 'description', 'created_at', 'status']
+        fields = ['uid', 'name', 'type', 'description',
+                  'created_by', 'created_at', 'status']
         extra_kwargs = {'name': {'required': True}, 'type': {
             'required': True}, 'description': {'required': True}}
