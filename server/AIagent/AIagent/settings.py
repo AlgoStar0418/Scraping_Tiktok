3b5a8f4513f354085ba0ec0d9d19b9df6a87033c
AlgoStar0418 <bs07120418@gmail.com>
2023-10-03 10:36:15 +0200

    [update]

--- server/AIagent/AIagent/settings.py
+++ server/AIagent/AIagent/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'aiagent',
         'USER': 'postgres',
-        'PASSWORD': 'KALISA123.',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '',
     }
